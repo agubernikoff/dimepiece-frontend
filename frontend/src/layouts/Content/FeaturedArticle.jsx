aa1a1e9592cedbf050e6ea72a0530ac375bb7c55
alexander gubernikoff <agubernikoff@gmail.com>
2023-10-17 19:06:29 -0400

    button style

--- frontend/src/layouts/Content/FeaturedArticle.jsx
+++ frontend/src/layouts/Content/FeaturedArticle.jsx
@@ -9,7 +9,7 @@ function FeaturedArticle() {
            <div className="featured-story-blurb-container-inside">
             <h2 className="featured-story-preview-title">Ginny Wright Makes Every Minute Count</h2>
             <p className="featured-story-preview-description">The CEO of Audemars Piguet and I were scheduled to meet on a sunny, sultry day in late August, but it was cool and crisp inside AP House, the new(ish) appointment only concept space by Audemars Piguet.</p>
-            <button className="read-story-btn">Read Story</button>
+            <button className="read-story-btn">READ STORY</button>
           </div>
         </div>
     </div>
