dc586afa78749f97587b92c0d683d671a9e03990
alexander gubernikoff <agubernikoff@gmail.com>
2023-10-17 18:55:50 -0400

    featured story style

--- frontend/src/layouts/Content/FeaturedArticle.jsx
+++ frontend/src/layouts/Content/FeaturedArticle.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import Cobey from '../../assets/Cobey.png'
 
 function FeaturedArticle() {
   return (
     <div className="featured-story">
-        <img className="featured-story-img" alt="" src=""/>
+        <img className="featured-story-img" alt="***TO BE FIXED***" src={Cobey}/>
          <div className="featured-story-blurb-container">
-            <p className="featured-story-preview-label">INTERVIEW</p>
+           <div className="featured-story-blurb-container-inside">
             <h2 className="featured-story-preview-title">Ginny Wright Makes Every Minute Count</h2>
             <p className="featured-story-preview-description">The CEO of Audemars Piguet and I were scheduled to meet on a sunny, sultry day in late August, but it was cool and crisp inside AP House, the new(ish) appointment only concept space by Audemars Piguet.</p>
             <button className="read-story-btn">Read Story</button>
+          </div>
         </div>
     </div>
   )
