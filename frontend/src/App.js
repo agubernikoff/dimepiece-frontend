5fbbe4c173b4bdab71f65157287e22fc41f995c8
alexander gubernikoff <agubernikoff@gmail.com>
2023-10-16 20:53:18 -0400

    sanity client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 // import React, { Suspense, lazy } from 'react';
-import React, { Suspense } from 'react';
+import React, { useEffect,Suspense } from 'react';
 import './App.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import PageNotFound from './pages/PageNotFound';
 import Header from './layouts/Header';
 import Homepage from './layouts/Content/Homepage';
+import {client} from './sanity/SanityClient';
 
 // const Posts = lazy(() => import('./pages/Posts'));
 
 function App() {
+useEffect(() =>{client.fetch(`*[_type == "product"]`).then((response) =>console.log(response))});
   return (
     <Suspense
       fallback={
