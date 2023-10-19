b09101ea4fdbf38425dca96e879daf3d73c81b01
Sam Gubernikoff <sgubernikoff@gmail.com>
2023-10-19 00:50:19 -0400

    homepage work

---
+++ frontend/src/components/products/BrynnsPick.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { client } from "../../sanity/SanityClient";
+
+function BrynnsPick() {
+  const [featured, setFeatured] = useState([]);
+  useEffect(() => {
+    client
+      .fetch(`*[_type == "product" && isFeatured == true][0]`)
+      .then((response) => setFeatured(response));
+  }, []);
+
+  console.log(featured);
+  if (featured.store) {
+    return (
+      <div className="featured-watch">
+        <h3 className="section-title-home">{"SHOP BRYNN'S PICK"}</h3>
+        <p className="featured-watch-headline">{featured.featuredHeadline}</p>
+        <div className="featured-watch-content-container">
+          <div className="featured-watch-description">
+            <p>{featured.description}</p>
+            <button>View Product</button>
+          </div>
+          <div className="featured-watch-image">
+            <img src={featured.store.previewImageUrl} />
+          </div>
+          <div className="featured-watch-details">
+            <div className="featured-watch-detail-container">
+              <p>{featured.brand}</p>
+              <p>{featured.title}</p>
+              <p>${featured.store.priceRange.minVariantPrice}</p>
+            </div>
+            <div className="featured-watch-detail-container">
+              <h5>DATE OF BIRTH</h5>
+              <p>{featured.dateOfBirth}</p>
+            </div>
+            <div className="featured-watch-detail-container">
+              <h5>MATERIAL</h5>
+              <p>{featured.material}</p>
+            </div>
+            <div className="featured-watch-detail-container">
+              <h5>SIZE</h5>
+              <p>{featured.size}</p>
+            </div>
+            <div className="featured-watch-detail-container">
+              <h5>MOVEMENT</h5>
+              <p>{featured.movement}</p>
+            </div>
+            <div className="featured-watch-detail-container">
+              <h5>CONDITION</h5>
+              <p>{featured.condition}</p>
+            </div>
+            <div className="featured-watch-detail-container">
+              <h5>BOX/PAPERS</h5>
+              <p>{featured.boxPapers}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BrynnsPick;
--- frontend/src/layouts/Content/Homepage.jsx
+++ frontend/src/layouts/Content/Homepage.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import FeaturedArticle from './FeaturedArticle';
-import WatchPreview from '../../components/products/WatchPreview';
-import LatestsStories from '../../components/stories/LatestsStories';
+import React from "react";
+import FeaturedArticle from "./FeaturedArticle";
+import WatchPreview from "../../components/products/WatchPreview";
+import LatestsStories from "../../components/stories/LatestsStories";
+import BrynnsPick from "../../components/products/BrynnsPick";
 
 function Homepage() {
-    return (
-        <div className="homepage">
-            <FeaturedArticle/>
-            <LatestsStories/>
-            <WatchPreview/>
-        </div>
-    )
+  return (
+    <div className="homepage">
+      <FeaturedArticle />
+      <LatestsStories />
+      <WatchPreview />
+      <BrynnsPick />
+    </div>
+  );
 }
 
 export default Homepage;
