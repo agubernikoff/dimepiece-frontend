fb2b8e1e33e79ec7265ccee4f80807d85a98ba1e
alexander gubernikoff <agubernikoff@gmail.com>
2023-10-23 22:47:25 -0400

    stories page styling

--- frontend/src/layouts/Content/Stories.js
+++ frontend/src/layouts/Content/Stories.js
@@ -5,14 +5,21 @@ import { client } from "../../sanity/SanityClient";
 
 function Stories() {
   const [stories, setStories] = useState([]);
+  const [brynnsPick, setBrynnsPick] = useState();
   let filteredStories = [];
   const [category, setCategory] = useState("All");
   const categoryURLParam = useParams();
   const nav = useNavigate();
   useEffect(() => {
     client
+      .fetch(`*[_type == "product" && isFeatured == true][0]`)
+      .then((response) => setBrynnsPick(response));
+  }, []);
+
+  useEffect(() => {
+    client
       .fetch(
-        `*[_type == "articles"]{_id,title,category,_createdAt,coverImage{asset->{url}}} | order(_createdAt desc)`
+        `*[_type == "articles"]{_id,title,isFeatured,category,_createdAt,coverImage{asset->{url}}} | order(_createdAt desc)`
       )
       .then((response) => setStories(response));
   }, []);
@@ -47,8 +54,19 @@ function Stories() {
     "In The Field",
     "Interviews",
   ];
+  const activeStyle = ({ isActive }) =>
+    isActive
+      ? {
+          textDecoration: "underline",
+        }
+      : null;
   const mappedCategories = categories.map((c, i) => (
-    <NavLink key={i} to={`/stories/${c.replaceAll(" ", "-")}`}>
+    <NavLink
+      className="navbar-link"
+      style={activeStyle}
+      key={i}
+      to={`/stories/${c.replaceAll(" ", "-")}`}
+    >
       {c}
     </NavLink>
   ));
@@ -68,11 +86,42 @@ function Stories() {
     category === "All"
       ? stories.map((s) => <LatestStoriesCard key={s._id} story={s} />)
       : filteredStories.map((s) => <LatestStoriesCard key={s._id} story={s} />);
+  const mappedFeaturedTitles = [...stories]
+    .filter((s) => s.isFeatured)
+    .map((fs, i) => <li key={i}>{fs.title}</li>);
   return (
     <div className="stories-page">
-      <h3 className="section-title-home">LATEST STORIES</h3>
-      {mappedCategories}
-      {mappedStories}
+      <div className="stories-page-index">
+        <div className="stories-page-index-list">
+          <p>
+            <strong>CATEGORIES</strong>
+          </p>
+          {mappedCategories}
+        </div>
+        <div className="stories-page-index-list">
+          <p>
+            <strong>FEATURED</strong>
+          </p>
+          <ol>{mappedFeaturedTitles}</ol>
+        </div>
+        <div className="stories-page-index-list">
+          <p>
+            <strong>{"BRYNN'S PICK"}</strong>
+          </p>
+          <p className="stories-page-index-brynns-pick">
+            {brynnsPick.featuredHeadline}
+          </p>
+        </div>
+      </div>
+      <div className="stories-page-content">
+        <h3 className="section-title-home">LATEST STORIES</h3>
+        <p>
+          {
+            'Explore all past and present Dimpiece content, from in-depth interviews and 101 information like "What is a bezel?" "What size is good for my wrist?" "What other brands should be on my radar besides Rolex?" This is your gateway to the captivating universe of timepieces.'
+          }
+        </p>
+        <div className="stories-page-card-container">{mappedStories}</div>
+      </div>
     </div>
   );
 }
