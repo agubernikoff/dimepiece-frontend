e7999f78021ec99d3748bd834acbf3eef9ce9c82
alexander gubernikoff <agubernikoff@gmail.com>
2023-10-23 20:32:23 -0400

    featured article integration

--- frontend/src/layouts/Content/FeaturedArticle.jsx
+++ frontend/src/layouts/Content/FeaturedArticle.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { client } from "../../sanity/SanityClient";
 import Cobey from '../../assets/Cobey.png'
 
 function FeaturedArticle() {
-  return (
+  const [featured, setFeatured] = useState();
+
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type == "articles" && isFeatured == true][0]{_id,title,category,_createdAt,coverImage{asset->{url}}}`
+      )
+      .then((response) => setFeatured(response));
+  }, []);
+  console.log(featured)
+  if(featured)return (
     <div className="featured-story">
-      <img className="featured-story-img" alt="***TO BE FIXED***" src={Cobey}/>
+      <img className="featured-story-img" alt="***TO BE FIXED***" src={featured.coverImage.asset.url}/>
       <div className="featured-story-blurb-container">
         <div className="featured-story-blurb-container-inside">
-          <h2 className="featured-story-preview-title">Ginny Wright Makes Every Minute Count</h2>
+          <h2 className="featured-story-preview-title">{featured.title}</h2>
           <p className="featured-story-preview-description">The CEO of Audemars Piguet and I were scheduled to meet on a sunny, sultry day in late August, but it was cool and crisp inside AP House, the new(ish) appointment only concept space by Audemars Piguet.</p>
           <button className="read-story-btn">READ STORY</button>
         </div>
