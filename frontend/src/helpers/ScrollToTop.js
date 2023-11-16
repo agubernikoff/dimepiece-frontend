e39e8f566ea7ebf39c61053a5d0fbc913e2595e4
alexander gubernikoff <agubernikoff@gmail.com>
2023-11-16 17:00:22 -0500

    reset

--- frontend/src/helpers/ScrollToTop.js
+++ frontend/src/helpers/ScrollToTop.js
@@ -1,9 +1,3 @@
 export function scrollToTop() {
   window.scrollTo(0, 0); // Scrolls to the top of the page
-  document.getElementsByTagName("html")[0].style = { overflow: "hidden" };
-  document.getElementsByTagName("body")[0].style = { position: "fixed" };
-  setTimeout(() => {
-    document.getElementsByTagName("html")[0].style.clear;
-    document.getElementsByTagName("body")[0].style.clear;
-  }, 500);
 }
