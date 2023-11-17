6bc16c828caa031f8bd495c60ae659a2ad91d466
alexander gubernikoff <agubernikoff@gmail.com>
2023-11-17 16:38:56 -0500

    remove console log

--- frontend/src/components/stories/LatestStoriesCard.js
+++ frontend/src/components/stories/LatestStoriesCard.js
@@ -7,7 +7,6 @@ function LatestStoriesCard({ story }) {
   const formattedDate = dateObject.toLocaleDateString("en-US", options);
   const nav = useNavigate();
 
-  console.log(formattedDate, dateObject, story.datePublished);
   return (
     <div
       className={
