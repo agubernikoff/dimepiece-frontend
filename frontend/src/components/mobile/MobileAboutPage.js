6d260508b14a8bfcf7a84ca5e1da7e884323160e
Sam Gubernikoff <sgubernikoff@gmail.com>
2023-11-17 00:12:15 -0500

    carousel mobile

--- frontend/src/components/mobile/MobileAboutPage.js
+++ frontend/src/components/mobile/MobileAboutPage.js
@@ -5,6 +5,13 @@ import MobilePressCard from "./MobilePressCard";
 import SanityArticleImage from "../../sanity/SanityArticleImage";
 import SanityEmailLink from "../../sanity/SanityEmailLink";
 import SanityExternalLink from "../../sanity/SanityExternalLink";
+import ap from "../../assets/ap.png";
+import breda from "../../assets/breda.png";
+import ebay from "../../assets/ebay.png";
+import foundwell from "../../assets/foundwell.png";
+import hodinkee from "../../assets/hodinkee.png";
+import jcrew from "../../assets/jcrew.png";
+import watchesandwonders from "../../assets/watchesandwonders.png";
 
 function MobileAboutPage() {
   const [about, setAbout] = useState();
@@ -66,6 +73,33 @@ function MobileAboutPage() {
             <p className="about-brynn-portrait-title-tobias">
               Dimepiece works with Brands, Businesses, and Individuals alike
             </p>
+          </div>
+          <div className="carousel-container">
+            <div className="gradient"></div>
+            <div className="slider">
+              <img id="brand1" src={ap} alt="AP" />
+              <img id="brand2" src={breda} alt="Breda" />
+              <img id="brand3" src={ebay} alt="Ebay" />
+              <img id="brand4" src={foundwell} alt="Foundwell" />
+              <img id="brand5" src={hodinkee} alt="Hodinkee" />
+              <img id="brand6" src={jcrew} alt="J Crew" />
+              <img
+                id="brand7"
+                src={watchesandwonders}
+                alt="Watches And Wonders"
+              />
+              <img id="brand1" src={ap} alt="AP" />
+              <img id="brand2" src={breda} alt="Breda" />
+              <img id="brand3" src={ebay} alt="Ebay" />
+              <img id="brand4" src={foundwell} alt="Foundwell" />
+              <img id="brand5" src={hodinkee} alt="Hodinkee" />
+              <img id="brand6" src={jcrew} alt="J Crew" />
+              <img
+                id="brand7"
+                src={watchesandwonders}
+                alt="Watches And Wonders"
+              />
+            </div>
           </div>
           <div>
             <p className="about-brynn-portrait-title"> PRESS</p>
