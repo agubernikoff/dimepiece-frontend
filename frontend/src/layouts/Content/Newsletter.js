09a7e202e6178756f96aadac77d538283a624320
Sam Gubernikoff <sgubernikoff@gmail.com>
2023-11-20 18:04:19 -0500

    newsletter function

--- frontend/src/layouts/Content/Newsletter.js
+++ frontend/src/layouts/Content/Newsletter.js
@@ -1,6 +1,28 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 function Newsletter() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_571yygo",
+        "template_84i2kjw",
+        e.target,
+        "WPUweZAoXmamBd_kZ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          // Add success message or further actions here
+        },
+        (error) => {
+          console.log(error.text);
+          // Add error handling here
+        }
+      );
+  };
   return (
     <div className="newsletter">
       <h3 className="section-title-home">NEWSLETTER</h3>
@@ -8,10 +30,10 @@ function Newsletter() {
         Sign up for the Dimepiece Newsletter to stay up to date on all our
         latest stories, products, and offerings.
       </p>
-      <div className="newsletter-input-container">
-        <input placeholder="Email Address" type="email"></input>
-        <button>Join</button>
-      </div>
+      <form className="newsletter-input-container" onSubmit={sendEmail}>
+        <input type="email" placeholder="Email Address" name="email"></input>
+        <button type="submit">Join</button>
+      </form>
     </div>
   );
 }
