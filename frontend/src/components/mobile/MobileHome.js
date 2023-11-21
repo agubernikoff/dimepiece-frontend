81f97955c93bd8d90eef9477d34038d64895b174
Sam Gubernikoff <sgubernikoff@gmail.com>
2023-11-21 01:54:43 -0500

    add background scroll

--- frontend/src/components/mobile/MobileHome.js
+++ frontend/src/components/mobile/MobileHome.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MobileBrynnsPick from "./MobileBrynnsPick";
 import MobileDialDimepiece from "./MobileDialDimepiece";
 import MobileStories from "./MobileStories";
@@ -7,6 +7,42 @@ import { motion } from "framer-motion";
 import MobileNewsletter from "./MobileNewsletter";
 
 function MobileHome() {
+  useEffect(() => {
+    const handleScroll = () => {
+      // Calculate scroll position
+      const scrollY =
+        window.scrollY ||
+        window.pageYOffset ||
+        document.documentElement.scrollTop;
+
+      // Calculate the total height of the page
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      // Calculate how far the user is from the bottom of the page
+      const scrollFromBottom = documentHeight - scrollY - windowHeight;
+
+      // Set a threshold to determine when to change the background color (e.g., 50 pixels from the bottom)
+      const threshold = 50;
+
+      // Check if user is within the threshold from the bottom
+      if (scrollFromBottom <= threshold) {
+        // Apply a different background color when scrolling past the end of the page
+        document.body.style.backgroundColor = "#d7ceff"; // Replace with your desired color
+      } else {
+        // Revert to the default background color when not at the end of the page
+        document.body.style.backgroundColor = ""; // Reset to default color
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <motion.div
       className="mobile-homepage"
