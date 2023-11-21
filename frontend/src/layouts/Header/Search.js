1f7d74537c9c6f09c6afc42e192b9eba9fd4fe7a
alexander gubernikoff <agubernikoff@gmail.com>
2023-11-21 03:46:14 -0500

    desktop search animation

--- frontend/src/layouts/Header/Search.js
+++ frontend/src/layouts/Header/Search.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { cartActions } from "../../redux/cart-slice";
+import { motion } from "framer-motion";
 
 function Search({ hideSearch }) {
   const ref = useRef();
@@ -51,7 +52,18 @@ function Search({ hideSearch }) {
     }
   }
   return (
-    <div className="search">
+    <motion.div
+      initial={{ y: "-100%" }}
+      animate={{ y: 0 }}
+      exit={{ y: "-100%" }}
+      transition={{
+        duration: 0.25,
+        ease: "linear",
+      }}
+      layout="size"
+      key="search"
+      className="search"
+    >
       <div className="search-container">
         <form className="search-input-container" onSubmit={handleSubmit}>
           <input
@@ -65,13 +77,20 @@ function Search({ hideSearch }) {
           <button>{String.fromCharCode(8594)}</button>
         </form>
         {searchResults.length > 0 ? (
-          <div className="suggestions-container">
+          <div
+            // initial={{ y: "-100%" }}
+            // animate={{ y: 0 }}
+            // exit={{ y: "-100%" }}
+            // transition={{ duration: 0.25, ease: "linear" }}
+            // key="suggestions-container"
+            className="suggestions-container"
+          >
             <p className="suggestion-title">SUGGESTIONS</p>
             <div>{mappedSuggestions}</div>
           </div>
         ) : null}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
--- frontend/src/layouts/Header/index.jsx
+++ frontend/src/layouts/Header/index.jsx
@@ -1,7 +1,7 @@
 // import { useDispatch } from 'react-redux';
 // import { selectPost, setPreEditPost } from '../../redux/actions';
 import React, { useState } from "react";
-import { useScroll, motion } from "framer-motion";
+import { useScroll, motion, AnimatePresence } from "framer-motion";
 import {
   NavLink,
   useSearchParams,
@@ -18,10 +18,10 @@ function Header() {
   const nav = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const loc = useLocation();
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
   const isArticleLoaded = useSelector((state) => state.article.isArticleLoaded);
-  const [displaySearch,setDisplaySearch]=useState(false)
+  const [displaySearch, setDisplaySearch] = useState(false);
   // const dispatch = useDispatch();
 
   // const handleAddClick = () => {
@@ -41,62 +41,96 @@ function Header() {
 
   //       console.log(logo);
   const { scrollYProgress } = useScroll();
-  const scrollProgress = isArticleLoaded ? scrollYProgress : 0
+  const scrollProgress = isArticleLoaded ? scrollYProgress : 0;
 
-  function toggleSearch(){
+  function toggleSearch() {
     setDisplaySearch(!displaySearch);
   }
-  function hideSearch(){
+  function hideSearch() {
     setDisplaySearch(false);
   }
 
   return (
     <div className="header">
-      <div className="navbar-container"><div className="navbar">
-    {loc.pathname.split("/").length >= 4 &&
-    loc.pathname.split("/")[1] === "stories" ? (
-      <motion.div
-        className="scroll-progress-bar"
-        style={{
-          scaleX: scrollProgress,
-        }}
-      />
-    ) : null}
-    <div className="navbar-left">
-      <NavLink onClick={hideSearch}
-        to="/stories/All" className="navbar-link">
-        STORIES
-      </NavLink>
-      <NavLink onClick={hideSearch}
-        to="/shop/All?filter+by=Latest+Arrivals"
-        className="navbar-link"
-        // onClick={(e) => {
-        //   if (loc.pathname.includes("/shop/")) e.preventDefault();
-        // }}
-      >
-        SHOP
-      </NavLink>
-      <NavLink  onClick={hideSearch} to="/about" className="navbar-link">
-        ABOUT
-      </NavLink>
-    </div>
-    <div className="navbar-center">
-      <img src={logo} className="navbar-image" onClick={() => {nav(`/`);hideSearch()}} />
-    </div>
-    <div className="navbar-right">
-      <motion.div style={{ originY: '0px' }} layout="position"><NavLink onClick={hideSearch}  to="/newsletter" className="navbar-link">
-        NEWSLETTER
-      </NavLink></motion.div>
-      <motion.p style={{ originY: '0px' }} layout="position"className="navbar-link" onClick={()=>{toggleSearch();dispatch(cartActions.setSearchResults(""))}}>
-        SEARCH
-      </motion.p>
-      <motion.p style={{ originY: '0px' }} layout="position" className="navbar-link" onClick={()=>{dispatch(cartActions.toggleDisplayCart());hideSearch()}}>
-        {`CART${cart.length>0?` (${cart.length})`:""}`}
-      </motion.p>
-    </div>
-  </div></div>
-      {displaySearch?<Search hideSearch={hideSearch}/>:null}
+      <AnimatePresence>
+        {displaySearch && <Search hideSearch={hideSearch} />}
+      </AnimatePresence>
+      <div className="navbar-container">
+        {loc.pathname.split("/").length >= 4 &&
+        loc.pathname.split("/")[1] === "stories" ? (
+          <motion.div
+            className="scroll-progress-bar"
+            style={{
+              scaleX: scrollProgress,
+            }}
+          />
+        ) : null}
+        <div className="navbar">
+          <div className="navbar-left">
+            <NavLink
+              onClick={hideSearch}
+              to="/stories/All"
+              className="navbar-link"
+            >
+              STORIES
+            </NavLink>
+            <NavLink
+              onClick={hideSearch}
+              to="/shop/All?filter+by=Latest+Arrivals"
+              className="navbar-link"
+            >
+              SHOP
+            </NavLink>
+            <NavLink onClick={hideSearch} to="/about" className="navbar-link">
+              ABOUT
+            </NavLink>
+          </div>
+          <div className="navbar-center">
+            <img
+              src={logo}
+              className="navbar-image"
+              onClick={() => {
+                nav(`/`);
+                hideSearch();
+              }}
+            />
+          </div>
+          <div className="navbar-right">
+            <motion.div style={{ originY: "0px" }} layout="position">
+              <NavLink
+                onClick={hideSearch}
+                to="/newsletter"
+                className="navbar-link"
+              >
+                NEWSLETTER
+              </NavLink>
+            </motion.div>
+            <motion.p
+              style={{ originY: "0px" }}
+              layout="position"
+              className="navbar-link"
+              onClick={() => {
+                toggleSearch();
+                dispatch(cartActions.setSearchResults(""));
+              }}
+            >
+              SEARCH
+            </motion.p>
+            <motion.p
+              style={{ originY: "0px" }}
+              layout="position"
+              className="navbar-link"
+              onClick={() => {
+                dispatch(cartActions.toggleDisplayCart());
+                hideSearch();
+              }}
+            >
+              {`CART${cart.length > 0 ? ` (${cart.length})` : ""}`}
+            </motion.p>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 
