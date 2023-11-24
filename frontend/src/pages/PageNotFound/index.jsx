931b303b1e65bf32bcec7a29d749e6cf6ce4b559
Sam Gubernikoff <sgubernikoff@gmail.com>
2023-11-24 14:08:05 -0500

    404

--- frontend/src/pages/PageNotFound/index.jsx
+++ frontend/src/pages/PageNotFound/index.jsx
@@ -1,24 +1,13 @@
-import { Box, Button, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
-    <Box
-      flexDirection={'column'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      display={'flex'}
-      // minHeight={'100vh'}
-    >
-      <div>
-        <Typography variant="h3">404 Page not found</Typography>
-      </div>
-      <div style={{ marginTop: 18 }}>
-        <Button component={Link} to="/" variant="contained">
-          Back to home
-        </Button>
-      </div>
-    </Box>
+    <div className="page-not-found">
+      <h3 className="fourohfour-text">404 PAGE NOT FOUND</h3>
+      <Link to="/">
+        <button className="fourohfour-button">BACK TO HOME</button>
+      </Link>
+    </div>
   );
 }
 
