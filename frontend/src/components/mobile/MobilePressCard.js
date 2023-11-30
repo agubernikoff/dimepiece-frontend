736efb2d057aa2f594864b023aa267259eb946ed
alexander gubernikoff <agubernikoff@gmail.com>
2023-11-30 00:23:35 -0500

    press links

--- frontend/src/components/mobile/MobilePressCard.js
+++ frontend/src/components/mobile/MobilePressCard.js
@@ -3,10 +3,25 @@ import React from "react";
 function MobilePressCard({ article }) {
   return (
     <div className="mobile-about-dimepiece-press">
-      <img alt={article.title} src={article.image.asset.url} />
+      <img
+        alt={article.title}
+        src={article.image.asset.url}
+        onClick={() => {
+          if (article.link)
+            window.open(`${article.link}`, "_blank", "noopener,noreferrer");
+        }}
+      />
       <div>
         <p className="blue">{article.brand}</p>
-        <p className="mobile-about-dimepiece-press-title">{article.title}</p>
+        <p
+          className="mobile-about-dimepiece-press-title"
+          onClick={() => {
+            if (article.link)
+              window.open(`${article.link}`, "_blank", "noopener,noreferrer");
+          }}
+        >
+          {article.title}
+        </p>
         <p className="mobile-about-dimepiece-description">
           {article.description}
         </p>
--- frontend/src/layouts/Content/PressCard.js
+++ frontend/src/layouts/Content/PressCard.js
@@ -3,10 +3,25 @@ import React from "react";
 function PressCard({ article }) {
   return (
     <div className="about-dimepiece-press">
-      <img alt={article.title} src={article.image.asset.url} />
+      <img
+        alt={article.title}
+        src={article.image.asset.url}
+        onClick={() => {
+          if (article.link)
+            window.open(`${article.link}`, "_blank", "noopener,noreferrer");
+        }}
+      />
       <div>
         <p className="blue">{article.brand}</p>
-        <p className="about-dimepiece-press-title">{article.title}</p>
+        <p
+          onClick={() => {
+            if (article.link)
+              window.open(`${article.link}`, "_blank", "noopener,noreferrer");
+          }}
+          className="about-dimepiece-press-title"
+        >
+          {article.title}
+        </p>
         <p className="dimepiece-press-text">{article.description}</p>
       </div>
     </div>
