9224df29fb097a030af8aff0bfd9bb8e256a9662
alexander gubernikoff <agubernikoff@gmail.com>
2023-12-03 14:49:17 -0500

    filter out featured article from latest stories

--- frontend/src/components/stories/LatestsStories.js
+++ frontend/src/components/stories/LatestsStories.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { client } from "../../sanity/SanityClient";
+import { useSelector } from "react-redux";
 import LatestStoriesCard from "./LatestStoriesCard";
 
 function LatestsStories() {
-  const [stories, setStories] = useState([]);
-  useEffect(() => {
-    client
-      .fetch(
-        `*[_type == "articles" && isFeatured != true && mostDiscussed != true]{_id,title,category,datePublished,coverImage{asset->{url}}} | order(datePublished desc)[0..7]`
-      )
-      .then((response) => setStories(response));
-  }, []);
-  const mapped = stories.map((story) => (
-    <LatestStoriesCard key={story._id} story={story} />
-  ));
+  const stories = useSelector((state) => state.article.stories);
+  const featuredId = useSelector((state) => state.article.featured._id);
+  const mapped = [...stories]
+    .filter((story) => story._id !== featuredId)
+    .slice(0, 8)
+    .map((story) => <LatestStoriesCard key={story._id} story={story} />);
   return (
     <div>
       <h3 className="section-title-home">LATEST STORIES</h3>
