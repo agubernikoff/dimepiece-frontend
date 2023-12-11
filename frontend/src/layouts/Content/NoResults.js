87cc243c7752c255049dedfcc3d88ece1aac4d9f
alexander gubernikoff <agubernikoff@gmail.com>
2023-12-11 18:17:28 -0500

    no results styling

--- frontend/src/layouts/Content/NoResults.js
+++ frontend/src/layouts/Content/NoResults.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NoResults() {
   return (
-    <p className="section-title-home no-results">
+    <p className="section-title-home">
       NO PRODUCTS WERE FOUND MATCHING YOUR SELECTION.
       <br /> PLEASE TRY AGAIN.
     </p>
