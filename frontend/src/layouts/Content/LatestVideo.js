965aa58c794c311941cdd33f53770911a5085297
Sam Gubernikoff <sgubernikoff@gmail.com>
2023-12-11 20:23:21 -0500

    update

--- frontend/src/layouts/Content/LatestVideo.js
+++ frontend/src/layouts/Content/LatestVideo.js
@@ -18,7 +18,7 @@ function LatestVideo() {
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </div>
